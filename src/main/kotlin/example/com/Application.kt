816cc5f1a3dad3a907396e519b41179f3faee298
Pedro Cruz <pedrotsband@gmail.com>
2024-07-19 01:25:48 -0300

    colocando um pouco do db
    colocando um pouco do db

--- src/main/kotlin/example/com/Application.kt
+++ src/main/kotlin/example/com/Application.kt
@@ -2,6 +2,35 @@ package example.com
 
 import example.com.plugins.*
 import io.ktor.server.application.*
+import org.jetbrains.exposed.sql.*
+
+
+
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.SchemaUtils.create
+import org.jetbrains.exposed.sql.SchemaUtils.drop
+
+
+
+object MembrosTable : Table() {
+    val id = integer("id").autoIncrement()
+    val nome = varchar("nome", 50)
+    val trabalho = integer("trabalho")
+    override val primaryKey = PrimaryKey(id)
+}
+
+object EventosTable : Table() {
+    val id = integer("id").autoIncrement()
+    val titulo = varchar("titulo", 50)
+    val descricao = varchar("descricao", 255)
+    val duracao = integer("duracao")
+    val horarioInicio = varchar("horarioInicio", 5)
+    val horarioTermino = varchar("horarioTermino", 5)
+    val diaDaSemana = varchar("diaDaSemana", 15)
+    override val primaryKey = PrimaryKey(id)
+}
+
+
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -19,10 +48,22 @@ val membros = mutableListOf(
     Membro(nome = "João", id = 0, trabalho = 40, eventos = emptyList()),
 )
 
+
+
+
 // Create Evento instances and associate Membro instances with them
 val eventos = mutableListOf(
     Evento("Reunião", "Reunião de status semanal", 0, 60, "10:00", "11:00", DiaDaSemana.SEGUNDA, emptyList()),
 )
+
+
+
+
+
+// Create Evento instances and associate Membro instances with them
+
+
+
 
 fun findEventoById(eventos: List<Evento>, id: Int): Evento? {
     return eventos.find { it.id == id }
